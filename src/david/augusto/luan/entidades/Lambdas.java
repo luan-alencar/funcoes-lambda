845ae1af0152849d6e8aabe814fe96116b9b54e2
luan-alencar <luanalencar134@gmail.com>
2020-12-04 12:23:39 -0300

    -> Thread com função lambda <-

--- src/david/augusto/luan/entidades/Lambdas.java
+++ src/david/augusto/luan/entidades/Lambdas.java
@@ -11,9 +11,16 @@ public class Lambdas {
 			public void run() {
 				// Thread e um processo que trabalha como um subsistema,
 				// sendo uma forma de um processo se autodividir em duas ou mais tarefas.
-				System.out.println("Thread com classer interna!");
+				System.out.println("Thread com classer interna!" + "\n");
 			}
 		};
 		new Thread(runnable).start();
+
+		System.out.println("---------------------------------------------" + "\n");
+
+		Runnable runnable2 = () -> System.out.println("Thread com função lambda!");
+		new Thread(runnable2).start();
+
+		System.out.println("---------------------------------------------" + "\n");
 	}
 }
