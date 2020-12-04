e56e56b226ff0c1b00fd96ecd0b62cc5208ba0a2
luan-alencar <luanalencar134@gmail.com>
2020-12-04 16:13:07 -0300

    Add classe Pessoa

---
+++ src/david/augusto/luan/entidades/Pessoa.java
@@ -0,0 +1,33 @@
+package david.augusto.luan.entidades;
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+
+	public Pessoa(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public Pessoa() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
