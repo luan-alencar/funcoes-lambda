036e212196d4fcb146138a466ed5f480484f0b4b
luan-alencar <luanalencar134@gmail.com>
2020-12-04 13:42:44 -0300

    Utilizando forEach() para as instâncias das duas listas

--- src/david/augusto/luan/entidades/FuncoesLambdaComCollection.java
+++ src/david/augusto/luan/entidades/FuncoesLambdaComCollection.java
@@ -12,20 +12,14 @@ public class FuncoesLambdaComCollection {
 		System.out.println("Integer");
 		System.out.println("---------------------------------------------" + "\n");
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-
-		for (Integer i : list) {
-			System.out.println(i);
-		}
+		list.forEach(i -> System.out.println(i));
 
 		System.out.println("---------------------------------------------" + "\n");
 
 		System.out.println("String");
 		System.out.println("---------------------------------------------" + "\n");
 		List<String> list2 = Arrays.asList("Fulano 1", "Fulano 2", "Fulano 3", "Fulano 4", "Fulano 5");
-
-		for (String i2 : list2) {
-			System.out.println(i2);
-		}
+		list2.forEach(i2 -> System.out.println(i2));
 
 		System.out.println("---------------------------------------------" + "\n");
 
